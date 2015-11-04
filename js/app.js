fc5d3729016eee2c15d9c095e39d9c2d692580db
lisandro52 <lisandro52@hotmail.com>
2015-11-03 22:08:56 -0300

    Agregada mas informacion de productos

--- js/app.js
+++ js/app.js
@@ -7,11 +7,17 @@ angular.module('myApp', ['angular-table'])
 		vm.loadProducts = function() {
 			var httpRequest = $http({
 				method: 'GET',
-				url: 'https://dl.dropboxusercontent.com/u/21135903/clean_table.json',
+				url: 'https://dl.dropboxusercontent.com/u/21135903/clean_table_big.json',
 				data: ''
 			}).success(function(data, status) {
 				vm.products = data;
+				vm.total_productos = data.length;
 			});
+		};
+		
+		vm.config_at = {
+			itemsPerPage: 50,
+			fillLastPage: true
 		};
 		
 		vm.loadProducts();
